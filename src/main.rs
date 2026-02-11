b0dffb191b394b8655ac161f9e9bb4d6756d9180
TheSeaMacs <ahamedsimak@gmail.com>
2026-02-12 00:37:29 +0600

    Added automatic wallpaper switching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use image::GenericImageView;
 use kmeans_colors::get_kmeans_hamerly;
 use palette::{FromColor, Lab, Srgb};
+use std::fmt::format;
 use std::fs;
 use std::io;
 use std::process::Command;
+
+fn restart_waybar() -> std::io::Result<()> {
+    let _ = Command::new("pkill").arg("waybar").status();
+
+    let home = std::env::var("HOME").unwrap();
+
+    Command::new("waybar")
+        .arg("-c")
+        .arg(format!("{}/.config/waybar/return.jsonc", home))
+        .arg("-s")
+        .arg(format!("{}/.config/waybar/return.css", home))
+        .spawn()?;
+
+    println!("Waybar Started With new colors");
+
+    Ok(())
+}
 
 fn update_waybar_theme(bg: &str, fg: &str) -> std::io::Result<()> {
     let home = std::env::var("HOME").unwrap();
@@ -34,6 +52,23 @@ fn update_waybar_theme(bg: &str, fg: &str) -> std::io::Result<()> {
     Ok(())
 }
 
+fn update_wallpaper(path: &str) -> std::io::Result<()> {
+    let abs_path = std::fs::canonicalize(path)?;
+    let path_str = abs_path.to_str().unwrap();
+
+    let arg = format!(", {}, cover", path_str);
+
+    let status = Command::new("hyprctl")
+        .args(["hyprpaper", "wallpaper", &arg])
+        .status()?;
+
+    if status.success() {
+        println!("Successfully updated wallpaper!");
+    }
+
+    Ok(())
+}
+
 fn main() {
     let mut path = String::new();
 
@@ -55,10 +90,11 @@ fn main() {
             Lab::from_color(srgb)
         })
         .collect();
+
     let result = get_kmeans_hamerly(2, 20, 0.005, false, &pixels, 42);
     let color_1 = Srgb::from_color(result.centroids[0]);
     let color_2 = Srgb::from_color(result.centroids[1]);
-    println!("Colors extracted!");
+
     let bg = format!(
         "#{:02x}{:02x}{:02x}",
         (color_1.red * 255.0) as u8,
@@ -72,19 +108,11 @@ fn main() {
         (color_2.green * 255.0) as u8,
         (color_2.blue * 255.0) as u8
     );
+
+    println!("Colors extracted!");
+
     update_waybar_theme(&bg, &fg).expect("Failed to update waybar");
     println!("Updating waybar successful!");
-
-    let _ = Command::new("pkill").arg("waybar").status();
-
-    let home = std::env::var("HOME").unwrap();
-
-    Command::new("waybar")
-        .arg("-c")
-        .arg(format!("{}/.config/waybar/return.jsonc", home))
-        .arg("-s")
-        .arg(format!("{}/.config/waybar/return.css", home))
-        .spawn()
-        .expect("Could not run waybar");
-    println!("Waybar Started With new colors");
+    update_wallpaper(path).expect("Failed to update hyprpaper");
+    restart_waybar().expect("Failed to restart waybar");
 }
